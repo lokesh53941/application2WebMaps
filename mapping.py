f19d35d9e7919e19f2f3e513c9d9f7c3dcb113fa
lokesh53941 <65780262+lokesh53941@users.noreply.github.com>
2020-07-07 20:12:37 +0530

    WorldmapApplicatin2Udemy

---
+++ mapping.py
@@ -0,0 +1,32 @@
+import folium
+import pandas
+import json
+data=pandas.read_csv("C:\Lokesh\Repositories\webMapsApplication2Udemy\Volcanoes.txt")
+lat=list(data["LAT"])
+lon=list(data["LON"])
+elev=list(data["ELEV"])
+def colour_producer(elevation):
+    if elevation<1000:
+        return 'green'
+    elif 1000<=elevation<3000:
+        return 'blue'
+    else:
+        return 'red'
+
+map=folium.Map(location=[39.56,-86.34],zoom_start=6,tiles="Stamen Terrain")
+fgv=folium.FeatureGroup(name="volcanoes")
+
+for lt,ln,el in zip(lat, lon,elev):
+    fgv.add_child(folium.CircleMarker(location=[lt,ln],radius=6,popup=str(el)+"m",
+    fill_color=colour_producer(el),color='grey',fill_opacity=0.7))
+
+fgp=folium.FeatureGroup(name="population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json','r',encoding='utf-8-sig').read(),
+style_function=lambda x:{'fillColor':'green'if x['properties']['POP2005']<10000000
+else 'orange'if 10000000<=x['properties']['POP2005']<20000000 else 'red'}))
+
+map.add_child(fgv)
+map.add_child(fgp)
+map.add_child(folium.LayerControl())
+map.save("Map1.html")
